59c44e5741599fae31a18d3393187decd1365511
Kraken536 <abd.aa536@gmail.com>
2023-02-28 01:27:11 +0300

    details and list components were added in this version.

---
+++ src/app/details-pokemon/details-pokemon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-pokemon',
+  templateUrl: './details-pokemon.component.html',
+  styles: [
+  ]
+})
+export class DetailsPokemonComponent {
+
+}
