7a168ff25faede6d6c2563ee7be30a6c3c23d12d
Kraken536 <abd.aa536@gmail.com>
2023-02-27 18:01:14 +0300

    Our pokemons have been displayed on the scree using The Materialize css.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { POKEMONS } from './mock-pokemon-list';
 import { Pokemon } from './pokemon';
 
+
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokémons</h1>`
+  templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
 
   constructor(){}
 
   ngOnInit(): void{
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[1]);
   }
 
-  selectPokemon(pokemon: Pokemon){
-    console.log(`You've clixked on pokemon ${pokemon.name}`);
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    //const index: number = +(event.target as HTMLInputElement).value; // The sign "+" allows us to convert the value into a number.
+    try{
+      if(pokemon){
+        this.pokemonSelected = pokemon;
+        console.log(`You've clicked on pokemon ${pokemon.name}`);
+      }else{
+        console.log(`There is no Pokémon available at index ${pokemonId}.`);
+        this.pokemonSelected = pokemon;
+      }
+    }catch(error){
+      throw `There is no Pokémon available at index ${pokemonId}.`;
+    }
   }
 }
