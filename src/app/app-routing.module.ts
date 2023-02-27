a70cfd455dfa6dd0255611c6ed6dc769a06dd324
Kraken536 <abd.aa536@gmail.com>
2023-02-28 01:48:41 +0300

    List pokemon component has been initialized with its route and is now accessible using a link.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailsPokemonComponent } from './details-pokemon/details-pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pokemons', 
+    component: ListPokemonComponent
+  },
+  {
+    path: 'pokemon/:id',
+    component: DetailsPokemonComponent
+  },
+  {
+    path: '',
+    redirectTo: 'pokemons',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { POKEMONS } from '../mock-pokemon-list';
 
 @Component({
   selector: 'app-list-pokemon',
   templateUrl: './list-pokemon.component.html',
-  styles: [
-  ]
 })
 export class ListPokemonComponent {
+  
+  constructor(){ }
+
+  pokemonList: Pokemon[] = POKEMONS;
 
 }
