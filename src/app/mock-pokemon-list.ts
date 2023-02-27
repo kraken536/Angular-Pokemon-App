a748a3ef59377d8c2b3517e7a5fee9d42c9ab427
Kraken536 <abd.aa536@gmail.com>
2023-02-27 03:43:52 +0300

    The Pokemon's models were added in this version.

---
+++ src/app/mock-pokemon-list.ts
@@ -0,0 +1,113 @@
+import { Pokemon } from './pokemon';
+
+
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        name: "Bulbizarre",
+        hp: 25,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Plante", "Poisson"],
+        created: new Date()
+    },
+    {
+        id: 2,
+        name: "Salameche",
+        hp: 28,
+        cp: 6,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 3,
+        name: "Carapuce",
+        hp: 21,
+        cp: 4,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Aspicot",
+        hp: 16,
+        cp: 2,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Insecte", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 5,
+        name: "Roucool",
+        hp: 30,
+        cp: 7,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 6,
+        name: "Rattata",
+        hp: 18,
+        cp: 6,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 7,
+        name: "Piafabec",
+        hp: 14,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 8,
+        name: "Abo",
+        hp: 16,
+        cp: 4,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Poison"],
+        created: new Date()
+    },
+    {
+        id: 9,
+        name: "Pikachu",
+        hp: 21,
+        cp: 7,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Elektrik"],
+        created: new Date()
+    },
+    {
+        id: 10,
+        name: "Sabelette",
+        hp: 19,
+        cp: 3,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 11,
+        name: "Melofée",
+        hp: 25,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Fée"],
+        created: new Date()
+    },
+    {
+        id: 12,
+        name: "Groupix",
+        hp: 17,
+        cp: 8,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/",
+        types: ["Feu"],
+        created: new Date()
+    },
+];
