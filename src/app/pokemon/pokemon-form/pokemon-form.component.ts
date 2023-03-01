2bc72d432d51712eab2a074822dbbd728fb5f53b
Kraken536 <abd.aa536@gmail.com>
2023-03-01 23:59:17 +0300

    The forms are getting configured.

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-pokemon-form',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   ]
 })
 export class PokemonFormComponent {
+  pokemon: Pokemon;
 
+  onSubmit(){
+    
+  }
 }
