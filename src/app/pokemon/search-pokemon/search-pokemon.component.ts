8281f28338dae584a45ac9c70c40bf7151b53800
Kraken536 <abd.aa536@gmail.com>
2023-03-10 20:18:47 +0300

    The Search function was configured and added to the listComponent in this version.

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { Router } from '@angular/router';
+import { Subject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search-pokemon',
-  templateUrl: './search-pokemon.component.html',
-  styles: [
-  ]
+  templateUrl: './search-pokemon.component.html', 
 })
-export class SearchPokemonComponent {
+export class SearchPokemonComponent implements OnInit {
 
+  /*
+    Search terms represente les recherches successives de l'utilisateur 
+    dans le champs de recherche.  
+  */
+  searchTerms = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+  constructor(
+    private router: Router 
+  ){ }
+  
+  ngOnInit(): void {
+
+  }
+
+  search(term: string){
+    this.searchTerms.next(term);
+  }
+
+  goToDetailPokemon(pokemon: Pokemon){
+    const link = [`/pokemon/`, pokemon.id];
+    this.router.navigate(link);
+  }
 }
