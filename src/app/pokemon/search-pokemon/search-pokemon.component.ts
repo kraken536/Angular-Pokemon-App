448f523d8b1d486ad875b0c3965339d33a884f51
Kraken536 <abd.aa536@gmail.com>
2023-03-12 03:44:45 +0300

    The search component is now operational.

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../pokemon';
 import { Router } from '@angular/router';
-import { Subject, Observable, debounceTime } from 'rxjs';
+import { Subject, Observable, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 import { PokemonService } from '../pokemon.service';
 
 @Component({
@@ -24,6 +24,11 @@ export class SearchPokemonComponent implements OnInit {
   ngOnInit(): void {
     this.pokemons$ = this.searchTerms.pipe(
       debounceTime(300),
+      distinctUntilChanged(),
+      /*The switchMap will stop the current request and immediately 
+      start looking for the new one entered by the user
+      */
+      switchMap((term) => this.pokemonService.searchPokemonList(term)),
     )
   }
 
