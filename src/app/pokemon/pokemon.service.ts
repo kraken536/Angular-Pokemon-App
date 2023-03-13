13020a11aa4fbd05995b4c660c86e7b17a7b2e50
Kraken536 <abd.aa536@gmail.com>
2023-03-13 16:32:24 +0300

    A comment has been added to give more details about the improvements.

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -29,6 +29,7 @@ export class PokemonService {
     if(term.length <= 1){
       return of([]); //This line will return an empty array for the condition.
     }
+    //The condition above is used to reduce the amount of server requests when the search term is not complete.
     return this.httpClient.get<Pokemon[]>(`api/pokemon/?name=${term}`)
     .pipe(
       tap((response)=> this.log(response)),
