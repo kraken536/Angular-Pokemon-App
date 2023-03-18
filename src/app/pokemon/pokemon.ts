1125c5dd77013ea28a878aa4e767ca79b8f83c1b
Kraken536 <abd.aa536@gmail.com>
2023-03-18 16:49:37 +0300

    Small change.

--- src/app/pokemon/pokemon.ts
+++ src/app/pokemon/pokemon.ts
@@ -14,7 +14,7 @@ export class Pokemon{
         cp = 10,
         picture: string = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/xxx.png',
         types: string[] = ["Normal"],
-        created: Date = new Date()
+        created: Date = new Date(),
         ){
             this.name = name;
             this.hp = hp;
